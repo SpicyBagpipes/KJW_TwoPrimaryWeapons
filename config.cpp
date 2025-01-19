e18211403dc99937c8bcdf32723541127824ef96
Dart <59131299+DartRuffian@users.noreply.github.com>
2025-01-19 11:05:16 -0600

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -61,11 +61,11 @@ class Extended_PreInit_EventHandlers {
 
 class CfgVehicles
 {
-	class CargoNet_01_box_F;
-	class GVAR(GWH): CargoNet_01_box_F
-	{
+    class CargoNet_01_box_F;
+    class GVAR(GWH): CargoNet_01_box_F
+    {
         scope = 1;
-		model="\A3\Weapons_f\DummyWeapon_Single.p3d";
-		showWeaponCargo=1;
-	};
+        model = "\A3\Weapons_f\DummyWeapon_Single.p3d";
+        showWeaponCargo = 1;
+    };
 };
